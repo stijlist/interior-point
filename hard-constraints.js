13380d15f3d7ee45c9d6a1c6532ed4f185e48ad5
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-09-11 21:53:14 -0400

    about to blow all this away

--- hard-constraints.js
+++ hard-constraints.js
@@ -1,57 +1,28 @@
-
-// solve hard constraints in code
-//
-// simplest solution: brute force search the solution space?
-//
-// imagine a simple constraint system
-//
-// constraint: we have two variables, and we need them to be equal
-//
-// x = y
-// how would we want to program this? 
-// the user of the constraint solver would simply want to declare: 
-eq(x, y);
-// and be done with it.
-
-// what are other kinds of equality constraints that we'd want to declare?
-// maybe something like:
-eq(plus(x, 10), y);
-// we can't simply do x + 10, because that will be evaluated immediately, and
-// we need to be continuously evaluating any relationship that isn't a direct
-// one to check that it's still satisfied. 
-// can we do this with arbitrary functions? for example, could the `plus`
-// function be user-defined? hmm. e.g:
-
-function plus(a1, a2) {
-    return function() { a1 + a2 };
-}
-
-// okay, we'd need to add anything we wanted to check to some stateful 
-// collection of constraints that the program knows about
-constraints = [];
-constraints.push(eq(plus(x, 10), y));
-
-// alright, let's write the eq function:
-function eq(a1, a2) {
-    return invoke_if_fn(var1) === invoke_if_fn(var2);
-}
-
-function invoke_if_fn(maybe_fn) {
-    return (typeof maybe_fn === 'function' ? maybe_fn() : maybe_fn);
-}
-
-// how would we check this?
-function check_constraints(constraint_list) {
-    // my instincts are SCREAMING that this is not going to be efficient enough
-    // it's fine, let's get something working
-    return constraints_list.filter(function(constraint) { 
-        return !constraint(); // filter for constraints that aren't satisfied
-    }).length == 0;
-} 
-
+// constraints are expressed in the form Ax = b
+// when we add a constraint, we want to add it to the equation that describes
+// the relationship between two variables
+// for example, if we have the variables box1.left and box2.right, and we want
+// box1.left to equal box2.right
+// we're trying to enforce the equation box1.left - box2.right = 0 
+// which can be added to a system of linear equations
+// what we're really after is if we can solve equations with 500+ constraints
+// in sub-millisecond time
 console.time("checking constraints: ")
 check_constraints(constraints);
 console.timeEnd("checking constraints: ")
 
-
-
+// so we need: a method of keeping track of how many constraints we're working
+// with. the matrix of constraints is nxn where n = number of variables
+// we have, and will grow until we're done adding constraints
+//
+// each constraint needs to be translated into an equation
+// is the mapping of constraints to equations 1-to-1? 
+//
+// constraints: a set of mappings from names to indices in the matrix & matrix
+// of coefficients that describe the relationships between names
+//
+// what do we do? maybe each constraint we get in 'add-constraint' will set all 
+// the variables it concerns to initial values that satisfy the constraint
+//
+//
+add_constraint('equal', box1.left, box2.right)
